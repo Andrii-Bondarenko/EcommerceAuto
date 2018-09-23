5f67314aee5821bffd872d98886c6ba8a84d8df0
Andrii-Bondarenko <bondarenko_andrii@outlook.com>
2018-09-23 17:36:24 +0300

    Payment page and quaranty

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -9,13 +9,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContentController extends Controller
 {
     /**
-     * @Route("/about", name="about")
-     */
+ * @Route("/about", name="about")
+ */
     public function about()
     {
         $currentItem['name'] = "О компании";
         $data['breadcrumbs'][] = $currentItem;
         return $this->render('pages/content/about.html.twig', ['data'=>$data]);
+    }
+
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function payment()
+    {
+        $currentItem['name'] = "Оплата";
+        $data['breadcrumbs'][] = $currentItem;
+        return $this->render('pages/content/payment.html.twig', ['data'=>$data]);
+    }
+
+    /**
+     * @Route("/guarantee", name="guarantee")
+     */
+    public function guarantee()
+    {
+        $currentItem['name'] = "Гарантии";
+        $data['breadcrumbs'][] = $currentItem;
+        return $this->render('pages/content/guarantee.html.twig', ['data'=>$data]);
     }
 }
 
