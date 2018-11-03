33169ca1e999fa35899550181785524002d737fb
Andrii-Bondarenko <bondarenko_andrii@outlook.com>
2018-11-03 12:02:30 +0200

    js load image product

--- public/js/common.js
+++ public/js/common.js
@@ -1,43 +1,66 @@
 $(document).ready(function () {
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav',
+    $(function(){
 
-    });
-    $('.slider-nav').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: false,
-        arrows: false,
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
+        function imageLoaded() {
+            counter--; // уменьшаем значение счетчика
+            if( counter === 0 ) {
+                $('.slider-for').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    fade: true,
+                    asNavFor: '.slider-nav',
+
+                });
+                $('.slider-nav').slick({
                     slidesToShow: 4,
                     slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
+                    asNavFor: '.slider-for',
+                    dots: false,
+                    arrows: false,
+                    focusOnSelect: true,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
             }
-        ]
+        }
+
+        var images = $('img'); // выбираем все изображения на странице
+        var counter = images.length; // количество изображений на странице
+
+        images.each(function() {
+            if( this.complete ) {
+                imageLoaded.call( this );
+            } else {
+                $(this).one('load', imageLoaded);
+            }
+        });
+
     });
+
+
+
 
 
     $('.cd-nav-trigger').on('click', function(event){
